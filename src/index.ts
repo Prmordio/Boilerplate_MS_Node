55b4b615fbf310a7a10061e78d60c7ec2c0b0a37
Ramon <ramon@prmord.io>
2024-02-25 11:40:20 -0400

    doc: configure lint & prettier

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import express from "express";
-import { Router, Request, Response } from "express";
+import express from 'express';
+import { Router, Request, Response } from 'express';
 
 const app = express();
 const route = Router();
 
 app.use(express.json());
 
-route.get("/", (req: Request, res: Response) => {
-  res.json({ message: "API ok", status: true });
+route.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'API ok', status: true });
 });
 
 app.use(route);
 
-app.listen(3333, () => "server running on port 3333");
+app.listen(3333, () => 'server running on port 3333');
