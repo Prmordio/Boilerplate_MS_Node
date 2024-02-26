fd7d613d40373fbb77285cf6edf61dbfc3f838a4
Ramon <ramon@prmord.io>
2024-02-26 10:04:05 -0400

    chore: failed push test

--- src/utils/sum.test.ts
+++ src/utils/sum.test.ts
@@ -2,7 +2,7 @@ import sum from './sum';
 
 describe('utils test', () => {
   test('should error sum', () => {
-    const soma = sum(1, 1);
+    const soma = sum(1, 2);
     expect(soma).toBe(2);
   });
 });
